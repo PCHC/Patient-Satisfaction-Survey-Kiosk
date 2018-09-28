6e846c64bde7929ab7c23f3d96e6befcb7e30ccb
Chris Violette <cvioletterun@gmail.com>
2018-09-28 13:20:37 -0400

    Added basic question routing & modeling

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Question;
 
 class QuestionsController extends Controller
 {
     public function index() 
     {
-        return view('questions.index');
+        $questions = Question::all();
+        return $questions;
+        // return view('questions.index', compact($questions));
+    }
+
+    public function show(Question $question)
+    {
+        return $question;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/survey', 'QuestionsController@index');
+
+Route::get('/questions/{question}', 'QuestionsController@show');
