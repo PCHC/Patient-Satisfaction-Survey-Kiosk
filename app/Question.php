8d5ea125f24b324a80eecdacaeccec0b7dca63dc
Chris Violette <cvioletterun@gmail.com>
2018-10-01 11:02:07 -0400

    Added documentation

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    /* Model whitelist: allow these fields to be mass-assigned */
     protected $fillable = ['question', 'active'];
+
+    /* Model blacklist: block these fields from being mass-assigned */
+    // protected $guarded = [];
 }
